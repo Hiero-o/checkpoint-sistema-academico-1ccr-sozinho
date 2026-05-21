f74a3ae5e57023f2f8d8f9f55b504fea0c416047
Gustavo Araujo Ramos da Silva <gustavoardss@gmail.com>
2026-05-21 01:02:11 -0300

    Adicionando menu de escolha

---
+++ main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+int main() {
+    printf("================================================\n");
+    printf("=                    SISTEMA XYZ                =\n");
+    printf("================================================\n");
+
+    printf("1 - Inserir Notas\n");
+    printf("2 Calcular média\n");
+    printf("3 - Verificar situação\n");
+    printf("4 - Exibir Resultado\n");
+    printf("5 - Caclcular derivada\n");
+    printf("6 - Sair\n");
+    printf("Digite um valor para continuar:\n")
+    int opcao
+    scanf("%d", &opcao);
+
+    return 0;
+}
